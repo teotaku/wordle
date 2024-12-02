1d9c7ecf9e261c7bdb10959c235f7ee2353a3948
teotaku <dongik9467@naver.com>
2024-12-02 13:42:20 +0900

    [워들] javaScript 로직 완성

--- js/index.js
+++ js/index.js
@@ -1,12 +1,84 @@
+const 정답 = "APPLE";
+
+let index = 0;
+let attempts = 0;
+
+const gameover = () => {
+  window.removeEventListener("keydown", handleEnterKey);
+  clearInterval(timer);
+};
+
 function appStart() {
+  const nextLine = () => {
+    if (attempts === 6) return gameover();
+    attempts++;
+    index = 0;
+  };
+
+  const handleEnterKey = () => {
+    let 맞은_갯수 = 0;
+    for (let i = 0; i < 5; i++) {
+      const block = document.querySelector(
+        `.board-column[data-index = '${attempts}${i}']`
+      );
+      const 입력한_글자 = block.innerText;
+      const 정답_글자 = 정답[i];
+      if (입력한_글자 === 정답_글자) {
+        맞은_갯수++;
+
+        block.style.background = "#6AAA64";
+      } else if (정답.includes(입력한_글자)) block.style.background = "#C9B458";
+      else block.style.background = "#787C7E";
+      block.style.color = "white";
+    }
+    if (맞은_갯수 === 5) gameover();
+    else nextLine();
+  };
+
+  const handleBackspace = () => {
+    if (index > 0) {
+      const preBlock = document.querySelector(
+        `.board-column[data-index = '${attempts}${index - 1}']`
+      );
+      preBlock.innerText = "";
+      if (index != 0) index -= 1;
+    }
+  };
+
+  const startTimer = () => {
+    const 시작시간 = new Date();
+
+    function setTime() {
+      const 현재시간 = new Date();
+      const 흐른시간 = new Date(현재시간 - 시작시간);
+
+      const 분 = 흐른시간.getMinutes().toString().padStart(2, "0");
+      const 초 = 흐른시간.getSeconds().toString().padStart(2, "0");
+      const timeDiv = document.querySelector("#timer");
+      timeDiv.innerText = `${분}:${초}`;
+    }
+
+    timer = setInterval(setTime, 1000);
+  };
+
   const handlekeyDown = (event) => {
     const key = event.key;
-    const keyCod = event.keyCod;
+    const keyCode = event.keyCode;
     const thisBlock = document.querySelector(
-      ".board-column[data-index = '00']"
+      `.board-column[data-index = '${attempts}${index}']`
     );
-    thisBlock.innerText = key;
+
+    if (event.key === "Backspace") handleBackspace();
+    else if (index === 5) {
+      if (event.key) handleEnterKey();
+      else return;
+    } else if (65 <= keyCode && keyCode <= 90) {
+      //keyCode가 a~z까지만
+      thisBlock.innerText = key.toUpperCase(); //대문자 변환
+      index++; // 로직돌리고 index 값 올리기
+    }
   };
+  startTimer();
   window.addEventListener("keydown", handlekeyDown);
 }
 
